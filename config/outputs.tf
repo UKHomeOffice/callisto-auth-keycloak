b52b56dd335d8c3a1de47044aba8f796cafad03b
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-07-21 09:48:10 +0100

    EAHW-1937 removed contents of outputs.tf, added not-prod tfvars file to main.yaml

--- config/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "repositories" {
-  value = data.github_team.callisto.repositories
-}
-
-
-output "unconfigured_team_repositories" {
-  value = setsubtract(data.github_team.callisto.repositories, [for k, v in local.repository_configs : k])
-}
