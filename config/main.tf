6da9a7cdf7cf3af3f8f9712f09ac65e9bde6969f
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-07-27 09:29:26 +0100

    EAHW-1937 fixed error with main.tf

--- config/main.tf
+++ config/main.tf
@@ -7,7 +7,7 @@ terraform {
 provider "keycloak" {
   # Variables in here must be defined locally before running the plan
   client_id = var.tf_client_id
-  client_secret = $var.tf_client_secret
+  client_secret = var.tf_client_secret
   realm = var.tf_realm
   url =var.tf_url
 }
