7203030cc32f922df70af87c82f26d85b00f9d96
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2022-07-28 15:45:44 +0100

    EAHW-1937 removed unneeded data and resources

--- config/main.tf
+++ config/main.tf
@@ -11,29 +11,3 @@ provider "keycloak" {
   realm         = var.tf_realm
   url           = var.tf_url
 }
-
-data "keycloak_realm" "callisto_notprod_auth" {
-  realm = var.tf_realm
-}
-
-
-resource "keycloak_openid_client" "callisto_ui" {
-  realm_id  = data.keycloak_realm.callisto_notprod_auth.id
-  client_id = "terraform-client"
-  name      = "Callisto SPA"
-  enabled   = true
-
-  access_type = "PUBLIC"
-
-  standard_flow_enabled        = true
-  direct_access_grants_enabled = false
-  service_accounts_enabled     = false
-  root_url                     = var.base_url
-  admin_url                    = var.base_url
-  web_origins = [
-    var.base_url
-  ]
-  valid_redirect_uris = [
-    "/*"
-  ]
-}
