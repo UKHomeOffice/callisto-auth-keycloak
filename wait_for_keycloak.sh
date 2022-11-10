1de2ffd4222fd0a6d1b935723844b0eec021d948
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-11-10 10:10:21 +0000

    update wait script to use timeout in foreground

--- wait_for_keycloak.sh
+++ wait_for_keycloak.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-which timeout
+timeout --foreground 120s < (docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on")
 # set -m
 # (sleep 120 && kill -s INT $$ && exit 1) &
 # mysleep=$!
