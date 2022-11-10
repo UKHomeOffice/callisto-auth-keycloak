a2463c97511c05fd8f6a6cf18cd9b7db8942e828
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-11-10 10:36:01 +0000

    Added echo after sleep to see if the command is being processed

--- wait_for_keycloak.sh
+++ wait_for_keycloak.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
 # kill -s INT $$
-(sleep 120 && kill -s INT $$ && exit 1) &
+(sleep 120 && echo Timed out waiting for Keycloak && kill -s INT $$ && exit 1) &
 mysleep=$!
 docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
 trap "kill $(jobs -p)" INT TERM HUP QUIT EXIT
