80948762c45972732fa077ff5dfc79b7c0ba62a0
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-11-10 10:18:51 +0000

    Reverted back to using script to background a sleep and investigate tty issue

--- wait_for_keycloak.sh
+++ wait_for_keycloak.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
-kill -s INT $$
-# set -m
-# (sleep 120 && kill -s INT $$ && exit 1) &
-# mysleep=$!
 # docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
-# trap "kill $(jobs -p)" INT TERM HUP QUIT EXIT
+# kill -s INT $$
+set -m
+(sleep 120 && kill -s INT $$ && exit 1) &
+mysleep=$!
+docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
+trap "kill $(jobs -p)" INT TERM HUP QUIT EXIT
