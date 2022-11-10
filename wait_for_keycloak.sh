2132df82e5d60504e02df46e1fca15df6429edbc
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-11-10 09:53:57 +0000

    Workflow and script to wait for keycloak to be ready as aservice

---
+++ wait_for_keycloak.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -m
+(sleep 120 && kill -s INT $$ && exit 1) &
+mysleep=$!
+docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
+trap "kill $(jobs -p)" INT TERM HUP QUIT EXIT
