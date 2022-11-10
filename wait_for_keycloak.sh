0d741255dda68a512aa3de438f68716db0c4c90f
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-11-10 11:07:05 +0000

    swapped sleep timings as they were the wrong way round

--- wait_for_keycloak.sh
+++ wait_for_keycloak.sh
@@ -7,7 +7,7 @@
 # docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on2"
 # trap "echo killing ;kill $(jobs -p)" INT TERM HUP QUIT EXIT
 
-sleep 60 && docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on" &
+sleep 10 && docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on" &
 loggrep=$!
-(sleep 3 && echo Timed out waiting for Keycloak && kill -9 $loggrep) &
+(sleep 60 && echo Timed out waiting for Keycloak && kill -9 $loggrep) &
 wait $loggrep
