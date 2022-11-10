48637b4b26069274f499de06daa2cfaa3d197bac
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-11-10 11:06:01 +0000

    Another attempt at script to wait for keycloak

--- wait_for_keycloak.sh
+++ wait_for_keycloak.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
 # kill -s INT $$
-(sleep 120 && echo Timed out waiting for Keycloak && kill -s INT $$ && exit 1) &
-mysleep=$!
-docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on"
-trap "kill $(jobs -p)" INT TERM HUP QUIT EXIT
+# set -m
+# (sleep 3 && echo Timed out waiting for Keycloak && kill -s INT $$ && exit 1) &
+# mysleep=$!
+# docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on2"
+# trap "echo killing ;kill $(jobs -p)" INT TERM HUP QUIT EXIT
+
+sleep 60 && docker logs -f $1 2> /dev/null | grep -m 1 "Admin console listening on" &
+loggrep=$!
+(sleep 3 && echo Timed out waiting for Keycloak && kill -9 $loggrep) &
+wait $loggrep
