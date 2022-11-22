d4e4c03174a40b5896c246dc66b7ad197463de3f
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-11-22 00:38:56 +0000

    Fmt corrections

--- config/outputs.tf
+++ config/outputs.tf
@@ -1,8 +1,8 @@
 output "realm_id" {
-    value = keycloak_realm.callisto.id
+  value = keycloak_realm.callisto.id
 }
 
 output "react_client_id" {
-    value = keycloak_openid_client.react_client.id
+  value = keycloak_openid_client.react_client.id
 }
 
